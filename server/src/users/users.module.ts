34039b63cb927583821cd779d8f3409bc8e07c73
RaviAnand Mohabir <moravrav@gmail.com>
2025-04-15 06:47:54 +0200

    fix: :bug: fix reference to user entity

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,6 +1,6 @@
 import { MikroOrmModule } from "@mikro-orm/nestjs";
 import { Module } from "@nestjs/common";
-import { User } from "../auth/auth.decorator";
+import { User } from "./user.entity";
 import { UsersResolver } from "./users.resolver";
 import { UsersService } from "./users.service";
 
