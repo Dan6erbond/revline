fb6dde99706d5aeb06806f83f49126b9a6a1c2de
RaviAnand Mohabir <moravrav@gmail.com>
2025-04-30 08:49:33 +0200

    feat: :sparkles: add Next.js website with App Router, TWCSS and HeroUI and Dockerfile

---
+++ website/src/app/navbar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import {
+  Button,
+  Navbar as HeroNavbar,
+  Link,
+  NavbarBrand,
+  NavbarContent,
+} from "@heroui/react";
+
+import { ArrowRight } from "lucide-react";
+import Wordmark from "./wordmark";
+
+export default function Navbar() {
+  return (
+    <HeroNavbar>
+      <NavbarContent>
+        <NavbarBrand as={Link} href="/">
+          <Wordmark />
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <Button
+          as="a"
+          href="/app/api/auth/signin/zitadel"
+          className="bg-white text-black hover:bg-gray-200 transition-colors flex items-center gap-2"
+        >
+          Log In <ArrowRight className="w-4 h-4" />
+        </Button>
+      </NavbarContent>
+    </HeroNavbar>
+  );
+}
