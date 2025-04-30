bb31b1989066f1d6a7ff6b29ef2ae451653ee9da
RaviAnand Mohabir <moravrav@gmail.com>
2025-04-30 14:23:00 +0200

    fix: :bug: include basepath in redirect

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,8 +1,10 @@
 import { auth } from "@/auth";
 
+const basePath = process.env.BASE_PATH ?? "";
+
 export default auth((req) => {
   if (!req.auth) {
-    const newUrl = new URL("/api/auth/signin", req.nextUrl.origin);
+    const newUrl = new URL(basePath + "/api/auth/signin", req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
 });
