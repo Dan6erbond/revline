6ee75f964a5d55cfd2b63dc55431a4ab062429e2
RaviAnand Mohabir <moravrav@gmail.com>
2025-04-30 17:13:27 +0200

    feat: :sparkles: include base path in car card

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -58,7 +58,7 @@ export default function Home() {
               key={car.id}
               isPressable
               as={Link}
-              href={`/cars/${car.id}/maintenance`}
+              href={href(`/cars/${car.id}/maintenance`)}
               className="overflow-hidden bg-primary-50/5 backdrop-blur hover:shadow-lg transition-shadow rounded-xl group"
             >
               <CardHeader className="relative h-[200px] w-full p-0 overflow-hidden">
