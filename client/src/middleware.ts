c0a850c6d3c8a99f84fbdb7de227e55366bfdde6
RaviAnand Mohabir <moravrav@gmail.com>
2025-05-02 12:12:36 +0200

    feat: :sparkles: exclude share urls from middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -3,7 +3,7 @@ import { auth } from "@/auth";
 const basePath = process.env.BASE_PATH ?? "";
 
 export default auth((req) => {
-  if (!req.auth) {
+  if (!req.auth && !req.nextUrl.pathname.startsWith("/share")) {
     const newUrl = new URL(basePath + "/api/auth/signin", req.nextUrl.origin);
     newUrl.searchParams.set("callbackUrl", basePath + req.nextUrl.pathname);
     return Response.redirect(newUrl);
