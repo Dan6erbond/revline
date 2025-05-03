7bd523538eaa73b0841a315dc524b9147665a4ff
RaviAnand Mohabir <moravrav@gmail.com>
2025-05-03 22:10:52 +0200

    ci: :construction_worker: use process.env.NEXT_PUBLIC_GRAPHQL_URL in codegen and run before build

--- client/codegen.ts
+++ client/codegen.ts
@@ -1,7 +1,8 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "http://localhost:4000/graphql",
+  schema:
+    process.env.NEXT_PUBLIC_GRAPHQL_URL ?? "http://localhost:4000/graphql",
   documents: ["src/**/*.{ts,tsx}"],
   generates: {
     "./src/gql/": {
