13ca978f87ef882445e9d8112cce2420ebd998e7
RaviAnand Mohabir <moravrav@gmail.com>
2025-05-07 16:31:47 +0200

    chore: :wrench: add generated files to globalIgnores

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,6 +1,8 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +11,9 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig = defineConfig([
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  globalIgnores("src/gql/"),
+]);
 
 export default eslintConfig;
