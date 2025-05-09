6acd7dc4b6bed8be48ba3b81a9386a12e1b6f8fa
RaviAnand Mohabir <moravrav@gmail.com>
2025-05-09 16:55:23 +0200

    feat: :sparkles: implement bulk import for dyno results, improve chart smoothness and use tabular view for dyno results

--- client/src/components/performance/dyno-sessions/session.tsx
+++ client/src/components/performance/dyno-sessions/session.tsx
@@ -3,13 +3,26 @@ import {
   Card,
   CardFooter,
   CardHeader,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Tooltip as HeroTooltip,
   Modal,
   ModalBody,
   ModalContent,
   ModalFooter,
   ModalHeader,
   NumberInput,
+  Selection,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
   Textarea,
+  cn,
   useDisclosure,
 } from "@heroui/react";
 import {
@@ -23,17 +36,27 @@ import {
   YAxis,
 } from "recharts";
 import { Controller, SubmitHandler, useForm } from "react-hook-form";
-import { Plus, Trash } from "lucide-react";
+import {
+  FilePlus,
+  Files,
+  Gauge,
+  Plus,
+  RotateCcw,
+  SlidersHorizontal,
+  Trash,
+} from "lucide-react";
+import { FormEventHandler, Key, ReactElement, useState } from "react";
 import { PowerUnit, TorqueUnit } from "@/gql/graphql";
 import { getKilowatts, getPower } from "@/utils/power";
 import { getNm, getTorque } from "@/utils/torque";
 import { powerUnitsShort, torqueUnitsShort } from "@/literals";
 import { useMutation, useSuspenseQuery } from "@apollo/client";
 
+import FancySwitch from "../../fancy-switch";
 import type { Payload } from "recharts/types/component/DefaultLegendContent";
 import { graphql } from "@/gql";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { withNotification } from "../../../utils/with-notification";
 
 const getDynoSession = graphql(`
   query GetDynoSession($id: ID!) {
@@ -76,6 +99,12 @@ const createDynoResult = graphql(`
   }
 `);
 
+const deleteDynoResults = graphql(`
+  mutation DeleteDynoResults($ids: [ID!]!) {
+    deleteDynoResults(ids: $ids)
+  }
+`);
+
 export default function Session() {
   const router = useRouter();
 
@@ -84,6 +113,8 @@ export default function Session() {
       id: router.query.tab![1],
     },
   });
+
+  const [selectedRows, setSelectedRows] = useState<Selection>(new Set());
 
   const powerUnit = data?.me?.profile?.powerUnit ?? PowerUnit.ImpHorsepower;
   const torqueUnit = data?.me?.profile?.torqueUnit ?? TorqueUnit.PoundFeet;
@@ -100,11 +131,21 @@ export default function Session() {
 
   const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure();
 
+  const [bulkAddType, setBulkAddType] = useState<string | ReactElement>("both");
+  const [bulkAddResults, setBulkAddResults] = useState("");
+
+  const {
+    isOpen: isBulkAddOpen,
+    onOpen: onBulkAddOpen,
+    onClose: onBulkAddClose,
+    onOpenChange: onBulkAddOpenChange,
+  } = useDisclosure();
+
   const { handleSubmit, control } = useForm<Inputs>({
     defaultValues: {},
   });
 
-  const [mutate] = useMutation(createDynoResult, {
+  const [mutate, { loading }] = useMutation(createDynoResult, {
     update(cache, res) {
       if (!res.data?.createDynoResult || !data.dynoSession) return;
 
@@ -127,6 +168,28 @@ export default function Session() {
     },
   });
 
+  const [mutateDelete, { loading: deleting }] = useMutation(deleteDynoResults, {
+    update(cache, res, { variables }) {
+      if (!res.data?.deleteDynoResults || !data.dynoSession) return;
+
+      cache.writeQuery({
+        query: getDynoSession,
+        variables: {
+          id: router.query.tab![1],
+        },
+        data: {
+          ...data,
+          dynoSession: {
+            ...data.dynoSession,
+            results: data.dynoSession.results?.filter(
+              (res) => !variables?.ids.includes(res.id)
+            ),
+          },
+        },
+      });
+    },
+  });
+
   const onSubmit: SubmitHandler<Inputs> = ({ rpm, power, torque }) => {
     mutate({
       variables: {
@@ -140,6 +203,64 @@ export default function Session() {
     }).then(() => {
       onClose();
     });
+  };
+
+  const handleAction = (action: Key) => {
+    switch (action) {
+      case "add":
+        onOpen();
+        break;
+      case "bulk-add":
+        onBulkAddOpen();
+        break;
+    }
+  };
+
+  const handleBulkAddSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    Promise.all(
+      bulkAddResults.split("\n").map((res) => {
+        let [rpm, power, torque] = ["", "", ""];
+
+        switch (bulkAddType) {
+          case "power":
+            [rpm, power] = res.split(",");
+            return mutate({
+              variables: {
+                input: {
+                  sessionID: router.query.tab![1],
+                  rpm: parseInt(rpm.trim()),
+                  powerKw: getKilowatts(parseInt(power.trim()), powerUnit),
+                },
+              },
+            });
+          case "torque":
+            [rpm, torque] = res.split(",");
+            return mutate({
+              variables: {
+                input: {
+                  sessionID: router.query.tab![1],
+                  rpm: parseInt(rpm.trim()),
+                  torqueNm: getTorque(parseInt(torque.trim()), torqueUnit),
+                },
+              },
+            });
+          case "both":
+            [rpm, power, torque] = res.split(",");
+            return mutate({
+              variables: {
+                input: {
+                  sessionID: router.query.tab![1],
+                  rpm: parseInt(rpm.trim()),
+                  powerKw: getKilowatts(parseInt(power.trim()), powerUnit),
+                  torqueNm: getTorque(parseInt(torque.trim()), torqueUnit),
+                },
+              },
+            });
+        }
+      })
+    ).then(onBulkAddClose);
   };
 
   return (
@@ -157,9 +278,33 @@ export default function Session() {
       </div>
       <div className="flex justify-between">
         <h3 className="text-xl">Results</h3>
-        <Button isIconOnly variant="bordered" radius="full" onPress={onOpen}>
-          <Plus />
-        </Button>
+        <Dropdown>
+          <DropdownTrigger>
+            <Button isIconOnly variant="bordered" radius="full">
+              <Plus />
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu
+            aria-label="Add results"
+            variant="faded"
+            onAction={handleAction}
+          >
+            <DropdownItem
+              key="add"
+              description="Input individual result"
+              startContent={<FilePlus />}
+            >
+              Add
+            </DropdownItem>
+            <DropdownItem
+              key="bulk-add"
+              description="Input multiple results"
+              startContent={<Files />}
+            >
+              Add bulk
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </div>
 
       <div className="aspect-video min-h-[300px] rounded-2xl bg-primary/5 backdrop-blur-xl px-6 md:px-10 py-8 md:py-12 border border-primary/10 shadow-sm">
@@ -170,8 +315,12 @@ export default function Session() {
                 ?.toSorted((a, b) => a.rpm - b.rpm)
                 .map(({ rpm, powerKw, torqueNm }) => ({
                   rpm,
-                  power: getPower(powerKw, powerUnit),
-                  torque: getTorque(torqueNm, torqueUnit),
+                  power:
+                    powerKw != null ? getPower(powerKw, powerUnit) : undefined,
+                  torque:
+                    torqueNm != null
+                      ? getTorque(torqueNm, torqueUnit)
+                      : undefined,
                 })) ?? []
             }
           >
@@ -231,6 +380,7 @@ export default function Session() {
               name={`Power (${powerUnitsShort[powerUnit]})`}
               hide={!visible.power}
               strokeOpacity={visible.power ? 1 : 0}
+              connectNulls
             />
             <Line
               yAxisId="right"
@@ -242,41 +392,101 @@ export default function Session() {
               name={`Torque (${torqueUnitsShort[torqueUnit]})`}
               hide={!visible.torque}
               strokeOpacity={visible.torque ? 1 : 0}
+              connectNulls
             />
           </LineChart>
         </ResponsiveContainer>
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
-        {data.dynoSession?.results
-          ?.toSorted((a, b) => a.rpm - b.rpm)
-          .map((r) => (
-            <Card key={r.id}>
-              <CardHeader className="flex justify-between">
-                <p>{r.rpm} RPM</p>
-                <Button
-                  isIconOnly
-                  variant="bordered"
-                  color="danger"
-                  size="sm"
-                  isDisabled
-                >
-                  <Trash size={16} />
-                </Button>
-              </CardHeader>
-              <CardFooter className="flex flex-col gap-2 items-start min-w-[200px]">
-                <p>
-                  {getPower(r.powerKw, powerUnit).toLocaleString()}{" "}
-                  {powerUnitsShort[powerUnit]}
-                </p>
-                <p>
-                  {getTorque(r.torqueNm, torqueUnit).toLocaleString()}{" "}
-                  {torqueUnitsShort[torqueUnit]}
-                </p>
-              </CardFooter>
-            </Card>
-          ))}
-      </div>
+      <Table
+        selectionMode="multiple"
+        selectedKeys={selectedRows}
+        onSelectionChange={setSelectedRows}
+        topContent={
+          <div className="flex items-center justify-between gap-4">
+            <div className="flex gap-2 items-center">
+              {selectedRows != "all" && selectedRows.size > 0 && (
+                <span>{selectedRows.size} rows selected</span>
+              )}
+            </div>
+            <div>
+              {(selectedRows === "all" || selectedRows.size > 0) && (
+                <HeroTooltip content="Delete">
+                  <Button
+                    variant="light"
+                    startContent={<Trash />}
+                    isIconOnly
+                    color="danger"
+                    size="sm"
+                    onPress={withNotification({}, () =>
+                      mutateDelete({
+                        variables: {
+                          ids:
+                            selectedRows === "all"
+                              ? data.dynoSession.results?.map(
+                                  (res) => res.id
+                                ) ?? []
+                              : ([...selectedRows] as string[]),
+                        },
+                      }).then(() => setSelectedRows(new Set()))
+                    )}
+                    isLoading={deleting}
+                  />
+                </HeroTooltip>
+              )}
+            </div>
+          </div>
+        }
+      >
+        <TableHeader
+          columns={[
+            { key: "rpm", name: "RPM" },
+            { key: "power", name: `Power (${powerUnitsShort[powerUnit]})` },
+            { key: "torque", name: `Torque (${torqueUnitsShort[torqueUnit]})` },
+            { key: "actions", name: "" },
+          ]}
+        >
+          {(column) => (
+            <TableColumn key={column.key}>{column.name}</TableColumn>
+          )}
+        </TableHeader>
+        <TableBody
+          items={
+            data.dynoSession.results?.toSorted((a, b) => a.rpm - b.rpm) ?? []
+          }
+        >
+          {(item) => (
+            <TableRow key={item.id}>
+              <TableCell>{item.rpm}</TableCell>
+              <TableCell>
+                {item.powerKw && getPower(item.powerKw, powerUnit)}
+              </TableCell>
+              <TableCell>
+                {item.torqueNm && getTorque(item.torqueNm, torqueUnit)}
+              </TableCell>
+              <TableCell className="flex items-center gap-2">
+                <HeroTooltip content="Delete">
+                  <Button
+                    variant="light"
+                    startContent={<Trash />}
+                    isIconOnly
+                    color="danger"
+                    size="sm"
+                    onPress={withNotification({}, () =>
+                      mutateDelete({
+                        variables: {
+                          ids: [item.id],
+                        },
+                      })
+                    )}
+                    isLoading={deleting}
+                  />
+                </HeroTooltip>
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
 
       <Modal
         isOpen={isOpen}
@@ -338,8 +548,98 @@ export default function Session() {
                 <Button color="danger" variant="light" onPress={onClose}>
                   Close
                 </Button>
-                <Button color="primary" type="submit" form="result-form">
+                <Button
+                  color="primary"
+                  type="submit"
+                  form="result-form"
+                  isLoading={loading}
+                >
                   Save
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+
+      <Modal
+        isOpen={isBulkAddOpen}
+        onOpenChange={onBulkAddOpenChange}
+        scrollBehavior="inside"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader>Bulk add</ModalHeader>
+              <ModalBody>
+                <form
+                  id="result-form"
+                  className="flex flex-col gap-4"
+                  onSubmit={handleBulkAddSubmit}
+                >
+                  <FancySwitch
+                    options={[
+                      {
+                        value: "power",
+                        label: powerUnitsShort[powerUnit],
+                        icon: <Gauge className="h-4 w-4" />,
+                      },
+                      {
+                        value: "torque",
+                        label: torqueUnitsShort[torqueUnit],
+                        icon: <RotateCcw className="h-4 w-4" />,
+                      },
+                      {
+                        value: "both",
+                        label: "Both",
+                        icon: <SlidersHorizontal className="h-4 w-4" />,
+                      },
+                    ]}
+                    value={bulkAddType}
+                    onChange={setBulkAddType}
+                    className="flex rounded-full bg-muted p-2"
+                    highlighterClassName="bg-primary rounded-full"
+                    radioClassName="relative mx-2 flex h-9 cursor-pointer items-center justify-center rounded-full px-3.5 text-sm font-medium transition-colors focus:outline-none data-[checked]:text-primary-foreground"
+                    highlighterIncludeMargin={true}
+                    renderOption={({ option, getOptionProps }) => (
+                      <div
+                        {...getOptionProps()}
+                        className={cn(
+                          "flex items-center gap-2",
+                          getOptionProps().className
+                        )}
+                      >
+                        <span>{option.icon}</span>
+                        <span>{option.label}</span>
+                      </div>
+                    )}
+                  />
+                  <Textarea
+                    variant="bordered"
+                    label="Results"
+                    placeholder={`Enter one result per line; in the format ${
+                      bulkAddType === "both"
+                        ? "rpm,power,torque"
+                        : bulkAddType === "power"
+                        ? "rpm,power"
+                        : "rpm,torque"
+                    }`}
+                    value={bulkAddResults}
+                    onChange={(e) => setBulkAddResults(e.target.value)}
+                  />
+                </form>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button
+                  color="primary"
+                  type="submit"
+                  form="result-form"
+                  isLoading={loading}
+                >
+                  Add
                 </Button>
               </ModalFooter>
             </>
