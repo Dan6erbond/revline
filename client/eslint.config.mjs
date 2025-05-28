5b2415d03a9c9a005122ccbe7261898027513bf2
RaviAnand Mohabir <moravrav@gmail.com>
2025-05-28 20:57:24 +0200

    chore(client): :wrench: add graphql eslint plugin

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import graphqlPlugin from "@graphql-eslint/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,6 +15,10 @@ const compat = new FlatCompat({
 const eslintConfig = defineConfig([
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   globalIgnores("src/gql/"),
+  {
+    files: ["**/*.js"],
+    processor: graphqlPlugin.processor,
+  },
 ]);
 
 export default eslintConfig;
