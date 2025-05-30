e44854222943374bd560a2371f9fbcc7c5aeccc5
RaviAnand Mohabir <moravrav@gmail.com>
2025-05-30 14:39:39 +0200

    fix: :bug: conditionally add remote patterns if env var is available

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,8 +1,27 @@
 import type { NextConfig } from "next";
 import { RemotePattern } from "next/dist/shared/lib/image-config";
 
-const s3Url = new URL("/**", process.env.S3_URL!);
-const apiUrl = new URL("/**", process.env.SERVER_URL!);
+const remotePatterns: RemotePattern[] = [];
+
+if (process.env.S3_URL) {
+  const s3Url = new URL("/**", process.env.S3_URL!);
+  remotePatterns.push({
+    protocol: s3Url.protocol.replace(":", "") as RemotePattern["protocol"],
+    hostname: s3Url.hostname,
+    port: s3Url.port,
+    pathname: s3Url.pathname,
+  });
+}
+
+if (process.env.SERVER_URL) {
+  const apiUrl = new URL("/**", process.env.SERVER_URL!);
+  remotePatterns.push({
+    protocol: apiUrl.protocol.replace(":", "") as RemotePattern["protocol"],
+    hostname: apiUrl.hostname,
+    port: apiUrl.port,
+    pathname: apiUrl.pathname,
+  });
+}
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
@@ -12,20 +31,7 @@ const nextConfig: NextConfig = {
     BASE_PATH: process.env.BASE_PATH,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: s3Url.protocol.replace(":", "") as RemotePattern["protocol"],
-        hostname: s3Url.hostname,
-        port: s3Url.port,
-        pathname: s3Url.pathname,
-      },
-      {
-        protocol: apiUrl.protocol.replace(":", "") as RemotePattern["protocol"],
-        hostname: apiUrl.hostname,
-        port: apiUrl.port,
-        pathname: apiUrl.pathname,
-      },
-    ],
+    remotePatterns,
   },
 };
 
