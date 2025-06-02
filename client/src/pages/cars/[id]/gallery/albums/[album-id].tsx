25865f589293da440bcbc9b53e6abc0d730d5049
RaviAnand Mohabir <moravrav@gmail.com>
2025-06-02 14:41:54 +0200

    fix: :bug: use next/dynamic and disable ssr due to infinite loading issue with suspense query

--- client/src/pages/cars/[id]/gallery/albums/[album-id].tsx
+++ client/src/pages/cars/[id]/gallery/albums/[album-id].tsx
@@ -1,8 +1,10 @@
-import AlbumView from "@/components/album";
 import CarLayout from "@/components/layout/car-layout";
 import { Suspense } from "react";
+import dynamic from "next/dynamic";
 import { getQueryParam } from "@/utils/router";
 import { useRouter } from "next/router";
+
+const AlbumView = dynamic(() => import("@/components/album"), { ssr: false });
 
 export default function Album() {
   const router = useRouter();
