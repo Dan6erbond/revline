8098a63a7e83acaba1ca892441a22b40af3f3837
RaviAnand Mohabir <moravrav@gmail.com>
2025-06-03 19:13:55 +0200

    feat: :sparkles: add build log to mod

--- client/src/mods/index.tsx
+++ client/src/mods/index.tsx
@@ -1,5 +1,13 @@
 import { ArrowLeft, Plus } from "lucide-react";
-import { Button, Input, Textarea, useDisclosure } from "@heroui/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  Divider,
+  Input,
+  Textarea,
+  useDisclosure,
+} from "@heroui/react";
 import { ModCategory, ModStatus } from "@/gql/graphql";
 import {
   categoryColors,
@@ -13,9 +21,12 @@ import { useMutation, useSuspenseQuery } from "@apollo/client";
 
 import { EnumSelect } from "@/components/enum-select";
 import Link from "next/link";
+import MediaItem from "@/components/media/item";
 import { ProductOptionCard } from "./product-option/card";
 import ProductOptionModal from "./product-option/modal";
 import { TaskCard } from "@/components/project/task";
+import { createExtensions } from "@/components/minimal-tiptap/hooks/use-minimal-tiptap";
+import { generateHTML } from "@tiptap/react";
 import { getQueryParam } from "@/utils/router";
 import { graphql } from "@/gql";
 import { useForm } from "react-hook-form";
@@ -53,6 +64,16 @@ export const getMod = graphql(`
       tasks {
         id
         ...TaskFields
+      }
+      buildLogs {
+        id
+        title
+        notes
+        media {
+          id
+          ...MediaItem
+        }
+        logTime
       }
     }
   }
@@ -117,10 +138,7 @@ export default function Mod({ id }: { id: string }) {
         </Button>
         <h1 className="text-2xl">Edit Mod</h1>
       </div>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-4"
-      >
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
         <Input label="Title" variant="bordered" {...register("title")} />
 
         <EnumSelect
@@ -194,6 +212,64 @@ export default function Mod({ id }: { id: string }) {
           <p>No tasks associated.</p>
         )}
       </div>
+
+      <section className="container mx-auto p-4 md:p-6">
+        <div className="flex justify-between">
+          <h1 className="mb-10 text-xl font-semibold">Build Log</h1>
+
+          {/* TODO: Implement add for mod */}
+          {/* <Button
+            startContent={<Plus />}
+            onPress={onOpen}
+            color="primary"
+            variant="flat"
+          >
+            Add
+          </Button> */}
+        </div>
+        <div className="relative mx-auto max-w-4xl">
+          <Divider
+            orientation="vertical"
+            className="absolute top-4 left-2 bg-content4"
+          />
+          {data?.mod?.buildLogs?.map((log) => (
+            <div key={log.id} className="relative mb-10 pl-8">
+              <div className="absolute top-3.5 left-0 flex size-4 items-center justify-center rounded-full bg-foreground" />
+
+              <h4 className="rounded-xl py-2 text-xl font-bold tracking-tight xl:mb-4 xl:px-3">
+                {log.title}
+              </h4>
+
+              <h5 className="text-md top-3 -left-34 rounded-xl tracking-tight text-muted-foreground xl:absolute">
+                {log.logTime ? new Date(log.logTime).toLocaleDateString() : ""}
+              </h5>
+
+              <Card className="my-5 border-none shadow-none">
+                <CardBody className="flex flex-col gap-4">
+                  <div
+                    className="prose text-foreground"
+                    dangerouslySetInnerHTML={{
+                      __html: generateHTML(log.notes, createExtensions("")),
+                    }}
+                  />
+
+                  {log.media && log.media.length > 0 && (
+                    <div className="flex flex-nowrap gap-4 overflow-x-auto p-2">
+                      {log.media?.map((m) => (
+                        <MediaItem
+                          item={m}
+                          key={m.id}
+                          className="aspect-square shrink-0"
+                        />
+                      ))}
+                    </div>
+                  )}
+                </CardBody>
+              </Card>
+            </div>
+          ))}
+        </div>
+      </section>
 
       <ProductOptionModal
         isOpen={isOpen}
