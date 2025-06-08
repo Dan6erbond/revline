7b16613753a1739cde57e5a4a4edec0c390d5098
RaviAnand Mohabir <moravrav@gmail.com>
2025-06-08 10:54:21 +0200

    fix: :bug: explicitly allow lodash/isEqual.js

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -6,5 +6,5 @@ export const config = {
   matcher: [
     "/((?!api|_next/static|_next/image|favicon.ico|favicon.png|favicon-96x96.png|web-app-manifest-192x192.png|web-app-manifest-512x512.png|manifest.json|apple-touch-icon.png).*)?",
   ],
-  unstable_allowDynamic: ["**/node_modules/lodash/isEqual.js"],
+  unstable_allowDynamic: ["lodash/isEqual.js"],
 };
