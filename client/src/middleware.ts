34e6aa43873ea0afd7a9d8c4ae09351f65ebbca4
RaviAnand Mohabir <moravrav@gmail.com>
2025-06-08 10:50:43 +0200

    fix(client): :bug: allow dynamic for auth due to usage of lodash/deepmerge

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -6,4 +6,5 @@ export const config = {
   matcher: [
     "/((?!api|_next/static|_next/image|favicon.ico|favicon.png|favicon-96x96.png|web-app-manifest-192x192.png|web-app-manifest-512x512.png|manifest.json|apple-touch-icon.png).*)?",
   ],
+  unstable_allowDynamic: ["/auth/apollo-client/index.js"]
 };
